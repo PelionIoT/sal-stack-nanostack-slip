b3a969bc474a3cf39c2c5720bf98f3d29170bc2b
Arto Kinnunen <Arto.Kinnunen@arm.com>
2018-12-05 14:25:22 +0200

    Replace deprecated attach method (#13)
    Replace deprecated mbed::SerialBase::attach(T*, void (T::*)() method
    with attach(callback(obj, method), type).

--- source/Slip.cpp
+++ source/Slip.cpp
@@ -88,7 +88,7 @@ int8_t SlipMACDriver::slip_if_tx(uint8_t *buf, uint16_t len, uint8_t tx_id, data
     _pslipmacdriver->pTxSlipBufferToTxFuncList.push(pTxBuf);
     core_util_critical_section_exit();
 
-    _pslipmacdriver->attach(_pslipmacdriver, &SlipMACDriver::txIrq, TxIrq);
+    _pslipmacdriver->attach(callback(_pslipmacdriver, &SlipMACDriver::txIrq), TxIrq);
 
     // success callback
     if( drv->phy_tx_done_cb ){
